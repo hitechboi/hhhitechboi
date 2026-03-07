321c6d2e745562eca33850abac0a4fe7f6ce9092
hitech <113853895+hitechboi@users.noreply.github.com>
2026-03-06 23:20:56 -0600

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -1,1 +1,9 @@
-
+local _a = game.GameId
+if _a == 6809578891 then
+    local user = game.Players.LocalPlayer.Name
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hitechboi/bizzarehijinks/refs/heads/main/hinjikse.lua"))()
+elseif _a == 73885730 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hitechboi/broccoli/refs/heads/main/mymoms.lua"))()
+else
+    notify("Check it", "This game is not supported.", 5)
+end
