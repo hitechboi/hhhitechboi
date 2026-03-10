fbe905b612913ffc0e0ec683e7f67b6b438d1bd8
hitech <113853895+hitechboi@users.noreply.github.com>
2026-03-10 01:33:35 -0500

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -1,6 +1,5 @@
 local _0xD=function(b)local r=""for i=1,#b do r=r..string.char(b[i])end return r end
 local _0x00=math.floor local _0x01=table.insert local _0x02=pairs local _0x03=ipairs local _0x04=pcall local _0x05=Vector3.new
-
 local _0x06={}local _0x07={}local _0x08=true local _0x09=os.clock()local _0x0A=0 local _0x0B={}local _0x0C=0 local _0x0E=10 local _0x0F=0
 local function _0x10()local _s={}_s._c={}
 function _s:Connect(_f)local _k={_fn=_f,_a=true}table.insert(_s._c,_k)return{Disconnect=function()_k._a=false _k._fn=nil end}end
@@ -18,13 +17,12 @@ if _0x08 then _0x06.RenderStepped:Fire(_d)end
 if _0x08 then _0x06.Heartbeat:Fire(_d)end end)
 if not _ok then _0x0F=_0x0F+1 if _0x0F>=_0x0E then _0x08=false break end else _0x0F=math.max(0,_0x0F-1)end
 if _0x08 then task.wait()end end end)
-
 local _a = game.GameId
 if _a == 6809578891 then
     local user = game.Players.LocalPlayer.Name
     loadstring(game:HttpGet(_0xD({104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,104,105,116,101,99,104,98,111,105,47,98,105,122,122,97,114,101,104,105,106,105,110,107,115,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,104,105,110,106,105,107,115,101,46,108,117,97,63,116,61})..os.time()))()
 elseif _a == 73885730 then
-    loadstring(game:HttpGet(_0xD({104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,104,105,116,101,99,104,98,111,105,47,98,114,111,99,99,111,108,105,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,109,121,109,111,109,115,46,108,117,97,63,116,61})..os.time()))()
+    notify("Check it", "Ui scrapped wait for a rework update", 5)
 else
     notify("Check it", "This game is not supported.", 5)
 end
