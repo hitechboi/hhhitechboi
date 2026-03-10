2e17aec2670cc8449e108744023a9bed1eb1d9dd
hitech <113853895+hitechboi@users.noreply.github.com>
2026-03-10 13:37:14 -0500

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -2,7 +2,7 @@ local gameId = game.GameId
 if gameId == 6809578891 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/hitechboi/bizzarehijinks/refs/heads/main/hinjikse.lua"))()
 elseif gameId == 73885730 then
-    notify("Check it", "Ui scrapped wait for a rework update", 5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hitechboi/checkitv2/refs/heads/main/prisonlife.lua"))()
 else
     notify("Check it", "This game is not supported.", 5)
 end
